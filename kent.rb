125f3ccf261b53fbfab9024c882b8aaae0f74bdc
sstandish <z.sisyphusrocks@gmail.com>
2018-06-26 16:56:49 -0700

    beginning to pull out the right content from the html

--- kent.rb
+++ kent.rb
@@ -10,16 +10,7 @@ content = open('https://kent.tedk12.com/hire/index.aspx')
 
 parsed_content = Nokogiri::HTML(content)
 
-# puts parsed_content.css('#JobList_1')
-
-
-i = 0
-5.times do
-  puts parsed_content.css('#JobList_#{i}')
-  i += 1
-end
-# this seems like an ineffecient way of going through these...there must be a better way...
-
+puts parsed_content.css('#JobList').inner_text
 
 
 # note: use . for classes and # for ids
