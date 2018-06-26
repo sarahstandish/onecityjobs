36d197ca1cb3e235d7bc65a83558879c211fd12b
sstandish <z.sisyphusrocks@gmail.com>
2018-06-26 16:06:11 -0700

    added Kent school district
    still not working too well

--- kent.rb
+++ kent.rb
@@ -10,4 +10,19 @@ content = open('https://kent.tedk12.com/hire/index.aspx')
 
 parsed_content = Nokogiri::HTML(content)
 
-puts parsed_content.css('.content').css('.panelContent').css('.DynaListGeneric').inner_html
+# puts parsed_content.css('#JobList_1')
+
+
+i = 0
+5.times do
+  puts parsed_content.css('#JobList_#{i}')
+  i += 1
+end
+# this seems like an ineffecient way of going through these...there must be a better way...
+
+
+
+# note: use . for classes and # for ids
+#.css('.panelContent')
+
+#.css('.content')
