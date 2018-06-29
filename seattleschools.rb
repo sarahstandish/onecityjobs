af88e4c5ece140dd2a66cd91d5635dc56fdf24b0
sstandish <z.sisyphusrocks@gmail.com>
2018-06-28 17:27:25 -0700

    still stuck

--- seattleschools.rb
+++ seattleschools.rb
@@ -1,13 +1,18 @@
-#starting a file to parse seattle schools web page 
+#starting a file to parse seattle schools web page
 
 require 'open-uri'
 require 'Nokogiri'
 
-content = open('https://www.governmentjobs.com/careers/seattleschools/')
+
+content = open('https://www.governmentjobs.com/careers/seattleschools?sort=PositionTitle%7CAscending')
 
 parsed_content = Nokogiri::HTML(content)
 
-parsed_content.css('.listing-title').css('.list-item')
+puts parsed_content
 
+#.css('#main-container') - gave me...something, but no jobs
 
-
+# note: use . for classes and # for ids
+# use .count to find out how many objects there are
+# watching: https://www.youtube.com/watch?v=1UYBAn69Qrk
+# .text - pull the text out of a nokogiri element
